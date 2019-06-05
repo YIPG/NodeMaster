2f1992b3340e265c2593487d836b4155c42a8b70
YIPG <around.mr.child@gmail.com>
2019-06-05 21:52:11 +0900

    added webworker threads

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const cluster = require("cluster")
+const express = require("express")
+const app = express()
+const crypto = require("crypto")
+const Worker = require("webworker-threads").Worker
 
-if (cluster.isMaster) {
-  cluster.fork()
-} else {
-  const express = require("express")
-  const app = express()
+app.get("/", (req, res) => {
+  const worker = new Worker(function() {
+    this.onmessage = function() {
+      let counter = 0
+      while (counter < 1e9) {
+        counter++
+      }
 
-  function doWork(duration) {
-    const start = Date.now()
-    while (Date.now() - start < duration) {}
+      postMessage(counter)
+    }
+  })
+
+  worker.onmessage = function(message) {
+    console.log(message.data)
+    res.send("" + message.data)
   }
 
-  app.get("/", (req, res) => {
-    doWork(5000)
-    res.send("Hi there")
-  })
+  worker.postMessage()
+})
 
-  app.get("/fast", (req, res) => {
-    res.send("Here is fast part")
-  })
-
-  app.listen(3000, () => console.log("now listing at localhost:3000"))
-}
+app.get("/fast", (req, res) => {
+  res.send("Here is fast part")
+})
+app.listen(3000, () => console.log("now listing at localhost:3000"))
